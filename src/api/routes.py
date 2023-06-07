e4e424966fce2a0f706556574f56a97199889cd8
Carlosgarciare2 <122974615+Carlosgarciare2@users.noreply.github.com>
2023-06-07 19:00:32 +0000

    Done

--- src/api/routes.py
+++ src/api/routes.py
@@ -75,15 +75,56 @@ def get_favorites():
     print(fav_data)
     return jsonify(fav_data)
 
-@api.route('/favorite/planet', methods=['POST'])
-def post_favorites():
-    data = request.json
 
-    planet = data.get('planet_id')
-    user = data.get('user_id')
-    if not user or not planet:
-        return jsonify({'message': "completa los campos requeridos"}), 404
-    
-    addFav = Favorites_planets(user_id=user, planet_id=planet)
-    db.session.add(addFav)
-    db.session.commit()
+@api.route('/favorite/planet/<int:planet_id>/<int:user_id>', methods=['POST'])
+def post_favorites(planet_id, user_id):
+    user = User.query.get(user_id)
+    if user:
+        favorite = Favorites_planets(user_id=user.id, planet_id=planet_id)
+        db.session.add(favorite)
+        db.session.commit()
+        return jsonify({'message': 'Favorite planet added successfully'})
+    else:
+        return jsonify({'message': 'User not found'}), 404
+
+
+@api.route('/favorite/character/<int: character_id>/<int:user_id>', method=['post'])
+def post_character(character_id, user_id):
+    user = User.query.get(user_id)
+    if user: 
+        favorite = Favorites_characters(user_id=user.id, character_id=character_id)
+        db.session.add(favorite)
+        db.session.commit()
+        return jsonify({'message': 'Favorite character added successfully'})
+    else:
+        return jsonify({'message': 'User not found'}), 404
+
+
+@api.route('/favorite/planet/<int:planet_id>/<int:user_id>', methods=['DELETE'])
+def delete_favorite_planet(planet_id, user_id):
+    user = User.query.get(user_id)
+    if user:
+        favorite = Favorites_planets.query.filter_by(user_id=user.id, planet_id=planet_id).first()
+        if favorite:
+            db.session.delete(favorite)
+            db.session.commit()
+            return jsonify({'message': 'Favorite planet deleted successfully'})
+        else:
+            return jsonify({'message': 'Favorite planet not found'}), 404
+    else:
+        return jsonify({'message': 'User not found'}), 404
+
+
+@api.route('/favorite/character/<int:character_id>/<int:user_id>', methods=['DELETE'])
+def delete_favorite_people(character_id, user_id):
+    user = User.query.get(user_id)
+    if user:
+        favorite = Favorites_characters.query.filter_by(user_id=user.id, character_id=character_id).first()
+        if favorite:
+            db.session.delete(favorite)
+            db.session.commit()
+            return jsonify({'message': 'Favorite people deleted successfully'})
+        else:
+            return jsonify({'message': 'Favorite people not found'}), 404
+    else:
+        return jsonify({'message': 'User not found'}), 404
